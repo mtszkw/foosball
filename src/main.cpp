d5530a83a9514d082e71932625e94bdb928cd7f0
Marcin Tomiec <33248000+mtomiec@users.noreply.github.com>
2018-03-13 00:51:03 +0100

    Added file selecting

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,31 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <windows.h>
+#include <windowsx.h>
 
 using namespace cv;
 using namespace std;
 
+HWND hwnd;              
+HANDLE hf;              
+
 int main()
 {
+	string path;
+	OPENFILENAME ofn;
+	char filename[MAX_PATH] = "";
+	ZeroMemory(&ofn, sizeof(ofn));
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = hwnd;
+	ofn.lpstrFilter = "Pliki wideo (*.avi)\0*.avi\0Wszystkie pliki\0*.*\0";
+	ofn.nMaxFile = MAX_PATH;
+	ofn.lpstrFile = filename;
+	ofn.lpstrDefExt = "avi";
+	ofn.Flags = OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
 	Mat frame;
-	string path = "E:\\Filmy\\c64.avi";
+	if (GetOpenFileName(&ofn)==TRUE)
+		path = ofn.lpstrFile;
 	VideoCapture capture(path);
 	namedWindow("my_window");
 
