f5726191f5c89fe0c0fbf2c3f6e37a1f4a2241ae
mtszkw <mtszkw@protonmail.com>
2018-03-18 18:22:49 +0100

    Add cxxopts library, fix CMake flags and input parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,53 @@
-#include <stdio.h>
+#include <iostream>
+#include <filesystem>
+
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
+#include "cxxopts.hpp"
 
-int main(int argc, char** argv ) {
-    Mat image;
-    image = imread( argv[1], 1 );
-    if ( !image.data ) {
-        printf("No image data \n");
-        return -1;
+cxxopts::ParseResult parseConfiguration(cxxopts::Options &options, int argc, const char *argv[])
+{
+    try
+    {
+        const auto config = options.parse(argc, argv);
+        if (!config.count("input_path") || config.count("help"))
+        {
+            std::cerr << options.help({ "" }) << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        return config;
+    }
+    catch (const cxxopts::OptionException& ex)
+    {
+        std::cerr << "FAILURE: Error while parsing options (" << ex.what() << ")\n";
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, const char *argv[])
+{
+    cxxopts::Options options(argv[0], "Implementacje Przemyslowe");
+    options.add_options("")
+        ("h,help", "Display help")
+        ("i,input_path", "Input video file path", cxxopts::value<std::string>());
+
+    const auto config = parseConfiguration(options, argc, argv);
+    const std::string input_path = config["input_path"].as<std::string>();
+
+    if (!std::experimental::filesystem::exists(input_path))
+    {
+        std::cerr << "FAILURE: Input file \"" << input_path << "\" does not exist.\n";
+        exit(EXIT_FAILURE);
     }
 
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    waitKey(0);
-    return 0;
+    cv::Mat image = cv::imread(input_path, 1);
+    if (!image.data)
+    {
+        std::cerr << "No image data.\n";
+        exit(EXIT_FAILURE);
+    }
+
+    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Display Image", image);
+    cv::waitKey(0);
 }
