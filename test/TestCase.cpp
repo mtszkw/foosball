44c754b7c3f8a7f4f03ca5eb013189af6aaadd7d
mtszkw <mtszkw@protonmail.com>
2018-03-20 10:42:57 +0100

    Update TestCase.cpp

--- test/TestCase.cpp
+++ test/TestCase.cpp
@@ -3,14 +3,3 @@
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
-
-int Factorial( int number ) 
-{
-    return number <= 1 ? number : Factorial( number - 1 ) * number;  // fail
-//  return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
-}
-
-TEST_CASE( "Factorial of 0 is 1 (fail)", "[single-file]" ) 
-{
-    REQUIRE( Factorial(0) == 1 );
-}
