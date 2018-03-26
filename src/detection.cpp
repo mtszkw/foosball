bb552b3d72bbc746ce383e980884d0c3266fecce
Urszula Krzeszewska <ulak.k@wp.pl>
2018-03-26 22:02:36 +0100

    Ball detection

---
+++ src/detection.cpp
@@ -0,0 +1,22 @@
+#include "detection.hpp"
+
+int main(int argc, char **argv) 
+{
+    cv::VideoCapture video(argv[1]);
+    cv::Mat frame;
+    
+    while(true)
+    {
+	video >> frame;
+	
+	cv::Mat hueImage = detection::transformToHSV(&frame);
+	detection::circlesDetection(&hueImage, &frame);
+	
+	cv::imshow("Detected circles", frame);
+	
+	if (cv::waitKey(30) == 27)
+	    break;
+    }
+
+    return 0;
+}
