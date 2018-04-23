f9a840a92babacb7a9076cc027f678cc20c861a6
raqiet <203942@edu.p.lodz.pl>
2018-04-23 12:35:14 +0200

    Refactor code

--- demo/calibration/main.cpp
+++ demo/calibration/main.cpp
@@ -29,18 +29,18 @@ cxxopts::ParseResult parseConfiguration(cxxopts::Options &options, int argc, con
 
 bool endsWith(std::string const & value, std::string const & ending)
 {
-	if (ending.size() > value.size()) return false;
-	return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
+    if (ending.size() > value.size()) return false;
+    return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
 }
 
 int main(int argc, const char *argv[])
 {
     cxxopts::Options options(argv[0], "Implementacje Przemyslowe");
     options.add_options("")
-        ("h,help", "Display help")
-        ("i,input_path", "Input settings file path", cxxopts::value<std::string>())
-        ("d,calibration_path", "Input camera calibration file", cxxopts::value<std::string>())
-        ("c,image_list_path", "Images to be undistored path", cxxopts::value<std::string>());
+    ("h,help", "Display help")
+    ("i,input_path", "Input settings file path", cxxopts::value<std::string>())
+    ("d,calibration_path", "Input camera calibration file", cxxopts::value<std::string>())
+    ("c,image_list_path", "Images to be undistored path", cxxopts::value<std::string>());
 
     const auto config = parseConfiguration(options, argc, argv);
     const std::string input_path = config["input_path"].as<std::string>();
@@ -54,54 +54,58 @@ int main(int argc, const char *argv[])
     }
 
     if(calibration_path.empty())
-	{
+    {
         calibration::CameraCalibration cameraCalibration(input_path);
         cameraCalibration.init();
-    } else 
-	{
+    }
+    else
+    {
         if(image_list_path.empty())
         {
-			calibration::CameraCalibration cameraCalibration(input_path, calibration_path);
-			namedWindow("DistortedImage", cv::WINDOW_NORMAL);
-			namedWindow("UndistortedImage", cv::WINDOW_NORMAL);
-	        for (auto & p : std::experimental::filesystem::directory_iterator(image_list_path)) {
-				if(endsWith(p.path().string(), ".MP4"))
-				{
-					cv::VideoCapture capture(p.path().string());
-					cv::Mat frame;
-					for (;;)
-					{
-						capture >> frame;
-						if (frame.empty())
-							break;
-						cv::imshow("DistortedImage", frame);
-						imshow("UndistortedImage", cameraCalibration.getUndistortedImage(frame));
-						if(char(cv::waitKey(5)) == 27)  // if ESC
-						{
-							break;
-						}
-					}
-				} else 
-				{
-					cv::Mat image = cv::imread(p.path().string(), CV_LOAD_IMAGE_COLOR);
-					cv::imshow("DistortedImage", image);
-					imshow("UndistortedImage", cameraCalibration.getUndistortedImage(image));
-					int keyCode;
-					while((keyCode = cv::waitKey(20)) == -1){}
-					if(char(keyCode) == 27)
-					{
-						break;
-					}
-				}
-			}
-        } else
-		{
-			std::cerr << "FAILURE: You have to provide image list path.\n";
-		}
-        
+            calibration::CameraCalibration cameraCalibration(input_path, calibration_path);
+            namedWindow("DistortedImage", cv::WINDOW_NORMAL);
+            namedWindow("UndistortedImage", cv::WINDOW_NORMAL);
+            for (auto & p : std::experimental::filesystem::directory_iterator(image_list_path))
+            {
+                if(endsWith(p.path().string(), ".MP4"))
+                {
+                    cv::VideoCapture capture(p.path().string());
+                    cv::Mat frame;
+                    for (;;)
+                    {
+                        capture >> frame;
+                        if (frame.empty())
+                            break;
+                        cv::imshow("DistortedImage", frame);
+                        imshow("UndistortedImage", cameraCalibration.getUndistortedImage(frame));
+                        if(char(cv::waitKey(5)) == 27)  // if ESC
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    cv::Mat image = cv::imread(p.path().string(), CV_LOAD_IMAGE_COLOR);
+                    cv::imshow("DistortedImage", image);
+                    imshow("UndistortedImage", cameraCalibration.getUndistortedImage(image));
+                    int keyCode;
+                    while((keyCode = cv::waitKey(20)) == -1) {}
+                    if(char(keyCode) == 27)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            std::cerr << "FAILURE: You have to provide image list path.\n";
+        }
+
     }
 
-    
+
 }
 
 
