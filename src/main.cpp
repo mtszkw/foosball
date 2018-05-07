f816d908b97ebfe2048cf5c8209308052a2c5702
Urszula Krzeszewska <ulak.k@wp.pl>
2018-05-07 10:17:35 +0200

    Updated files

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,6 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/aruco.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/video/video.hpp>
 
 #include "cxxopts.hpp"
 #include "aruco.hpp"
@@ -15,59 +12,52 @@
 #include "cameraCalibration.h"
 #include "detection.hpp"
 
-cxxopts::ParseResult parseConfiguration(cxxopts::Options &options, int argc, const char *argv[])
+int main(int argc, const char *argv[])
 {
+    cxxopts::Options options(argv[0], "Implementacje Przemyslowe");
+    options.add_options("")
+        ("help", "Display help")
+        ("input_path", "Input video file path",
+            cxxopts::value<std::string>())
+        ("aruco_path", "Aruco dictionary configuration",
+            cxxopts::value<std::string>()->default_value("data/dictionary.png"))
+        ("calibration", "Calibration config file",
+            cxxopts::value<std::string>()->default_value("data/out_camera_data.xml"))
+        ("aruco_conf", "Configuration of aruco detector",
+            cxxopts::value<std::string>()->default_value(""));
+
+    std::string INPUT_PATH, ARUCO_PATH, CALIB_PATH, ARUCO_CFG_PATH;
+
     try
     {
         const auto config = options.parse(argc, argv);
-        if (!config.count("input_path") || config.count("help"))
+        if (config.count("help"))
         {
-            std::cerr << options.help({ "" }) << std::endl;
-            exit(EXIT_FAILURE);
+            std::cout << options.help({ "" }) << std::endl;
+            exit(EXIT_SUCCESS);
         }
-        return config;
+        INPUT_PATH      = config["input_path"].as<std::string>();
+        ARUCO_PATH      = config["aruco_path"].as<std::string>();
+        CALIB_PATH      = config["calibration"].as<std::string>();
+        ARUCO_CFG_PATH  = config["aruco_conf"].as<std::string>();
     }
     catch (const cxxopts::OptionException& ex)
     {
         std::cerr << "FAILURE: Error while parsing options (" << ex.what() << ")\n";
         exit(EXIT_FAILURE);
     }
-}
 
-int main(int argc, const char *argv[])
-{
-    cxxopts::Options options(argv[0], "Implementacje Przemyslowe");
-    options.add_options("")
-        ("h,help", "Display help")
-        ("i,input_path", "Input video file path", cxxopts::value<std::string>())
-        ("d,aruco_path", "Aruco dictionary configuration", 
-            cxxopts::value<std::string>()->default_value("data/dictionary.png"))
-        ("c,calibration", "Calibration config file",
-            cxxopts::value<std::string>()->default_value("data/out_camera_data.xml"))
-        ("a,aruco_conf", "Configuration of aruco detector",
-            cxxopts::value<std::string>()->default_value(""));
-
-    const auto config = parseConfiguration(options, argc, argv);
-    const std::string input_path = config["input_path"].as<std::string>();
-    const std::string aruco_path = config["aruco_path"].as<std::string>();
-    const std::string calibration_path = config["calibration"].as<std::string>();
-    const std::string aruco_conf_path = config["aruco_conf"].as<std::string>();
-
-    if (!std::experimental::filesystem::exists(input_path))
-    {
-        std::cerr << "FAILURE: Input file \"" << input_path << "\" does not exist.\n";
-        exit(EXIT_FAILURE);
-    }
+    /////////////////////////////////////////////////////////////////////////////////////
 
     cv::Mat frame;
-    cv::VideoCapture capture(input_path);
+    cv::VideoCapture capture(INPUT_PATH);
 
-    cv::Ptr<cv::aruco::Dictionary> aruco_dict = aruco::createDictionary(aruco_path, 5);
-    cv::Ptr<cv::aruco::DetectorParameters> detector = aruco::loadParametersFromFile(aruco_conf_path);
+    cv::Ptr<cv::aruco::Dictionary> aruco_dict = aruco::createDictionary(ARUCO_PATH, 5);
+    cv::Ptr<cv::aruco::DetectorParameters> detector = aruco::loadParametersFromFile(ARUCO_CFG_PATH);
 
     std::vector<aruco::ArucoMarker> found, rejected;
 
-    calibration::CameraCalibration cameraCalibration("data/default.xml", calibration_path);
+    calibration::CameraCalibration cameraCalibration("data/default.xml", CALIB_PATH);
     table::Table gameTable(1200, 600); // Default table size
    
     detection::FoundBallsState foundBallsState(0.0, false, 0);
@@ -107,8 +97,10 @@ int main(int argc, const char *argv[])
 
         foundBallsState.clearVectors();
 
-        if (cv::waitKey(10) >= 0) 
-            break;
+        if (cv::waitKey(10) >= 0) break;
+
+        const int SKIP_FRAMES = 5;
+        for (int i = 0; i < SKIP_FRAMES; ++i) capture >> frame;
     }
 
     return 0;
