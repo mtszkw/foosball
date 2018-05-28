e8168c00f70ec50a87052235f919336b5c307b45
mtomiec <mtomiec@gmail.com>
2018-05-28 19:34:18 +0200

    Modified travis configuration

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: E:/Code_Formatting/travisCI/impl-przemyslowe
+# Build directory: E:/Code_Formatting/travisCI/impl-przemyslowe
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Test1 "E:/Code_Formatting/travisCI/impl-przemyslowe/Test.exe")
