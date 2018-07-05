6a2435a0ba6a13ce52a0d9727b9cbae3025a7b6e
mtszkw <mtszkw@protonmail.com>
2018-07-05 20:56:42 +0200

    Fix code indentation in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ using namespace std;
 nlohmann::json readConfiguration(const string &filename)
 {
     nlohmann::json config;
-	if (ifstream configFile(filename); configFile.is_open())
+    if (ifstream configFile(filename); configFile.is_open())
     {
         stringstream buffer;
         buffer << configFile.rdbuf();
@@ -59,7 +59,7 @@ int main()
     calibration::CameraCalibration cameraCalibration(config["calibInitConfigPath"].get<string>(),
                                                      config["calibConfigPath"].get<string>());
     if (config["calibConfigPath"].get<string>().empty())
-	{
+    {
         cameraCalibration.init();
     }
 
@@ -69,7 +69,7 @@ int main()
 
     int counter = 0, founded = 0; 
     detection::FoundBallsState foundBallsState(0.0, false, 0);
-	detection::PlayersFinder redPlayersFinder, bluePlayersFinder;
+    detection::PlayersFinder redPlayersFinder, bluePlayersFinder;
 
     // Initialize video capture object with video file and start processing
     cv::Mat frame, flippedFrame, nextFrame;
@@ -77,11 +77,11 @@ int main()
 
     while(capture.read(frame))
     {
-		gui::showOriginalFrame(originalEnabled, frame);
+        gui::showOriginalFrame(originalEnabled, frame);
 
         for (int i=0; i<config["videoSkipFramesStep"].get<int>(); ++i) capture >> frame;
 
-		capture.read(nextFrame);
+	capture.read(nextFrame);
         
         const double precTick = foundBallsState.getTicks();
         foundBallsState.setTicks(static_cast<double>(cv::getTickCount()));
@@ -96,23 +96,23 @@ int main()
         gameTable.updateTableOnFrame(found);
         frame = gameTable.getTableFromFrame(frame);
 		
-		aruco::detectArucoOnFrame(nextFrame, aruco_dict, found, rejected, detector);
+	aruco::detectArucoOnFrame(nextFrame, aruco_dict, found, rejected, detector);
         gameTable.updateTableOnFrame(found);
         nextFrame = gameTable.getTableFromFrame(nextFrame);
 
-		cv::Mat restul;
-		frame.copyTo(restul);
+	cv::Mat restul;
+	frame.copyTo(restul);
 
-		// Ball detection
-		detection::trackBall(trackingEnabled, debugMode, foundBallsState, deltaTicks, founded, counter, frame, nextFrame, restul);
+	// Ball detection
+	detection::trackBall(trackingEnabled, debugMode, foundBallsState, deltaTicks, founded, counter, frame, nextFrame, restul);
 		
-		// Players detection		
-		detection::detectPlayers(redDetectionEnabled, debugMode, detection::Mode::RED_PLAYERS, redPlayersFinder, frame, restul);
-		detection::detectPlayers(blueDetectionEnabled, debugMode, detection::Mode::BLUE_PLAYERS, bluePlayersFinder, frame, restul);
+	// Players detection		
+	detection::detectPlayers(redDetectionEnabled, debugMode, detection::Mode::RED_PLAYERS, redPlayersFinder, frame, restul);
+	detection::detectPlayers(blueDetectionEnabled, debugMode, detection::Mode::BLUE_PLAYERS, bluePlayersFinder, frame, restul);
         
-		cv::flip(restul, flippedFrame, 0);
+	cv::flip(restul, flippedFrame, 0);
 
-		// Calculate and show ball position and score
+	// Calculate and show ball position and score
         scoreCounter.trackBallAndScore(foundBallsState.getCenter(), foundBallsState.getFoundball()); 
         
         // Display GUI elements and score board
@@ -120,14 +120,16 @@ int main()
         gui::printScoreBoard(scoreCounter, flippedFrame, (int)(5.0 / 12 * config["gameTableWidth"].get<int>()), 30);
         gui::showCenterPosition(flippedFrame, foundBallsState.getCenter(), 10, config["gameTableHeight"].get<int>() + 65);
         gui::showStatistics(flippedFrame, founded, counter, 10, config["gameTableHeight"].get<int>() + 80);
-		gui::printKeyDoc(flippedFrame, 300, config["gameTableHeight"].get<int>() + 65);
-		cv::imshow("Foosball", flippedFrame);
+	gui::printKeyDoc(flippedFrame, 300, config["gameTableHeight"].get<int>() + 65);
+	cv::imshow("Foosball", flippedFrame);
 		
-		redPlayersFinder.clearVectors();
-		bluePlayersFinder.clearVectors();
+	redPlayersFinder.clearVectors();
+	bluePlayersFinder.clearVectors();
         foundBallsState.clearVectors();
 
-		gui::handlePressedKeys(cv::waitKey(10), originalEnabled, trackingEnabled, blueDetectionEnabled, redDetectionEnabled, pause, debugMode);
+	gui::handlePressedKeys(cv::waitKey(10), originalEnabled, trackingEnabled,
+			       blueDetectionEnabled, redDetectionEnabled, pause, debugMode);
     }
+	
     return 0;
 }
